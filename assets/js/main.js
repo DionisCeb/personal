51449b8719b435e619469b8af31709a9492fa8ef
Dionis <e2396498@cmaisonneuve.qc.ca>
2024-10-25 18:45:59 -0400

    refactoring the colors and adding the links

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,23 @@ import App from "./classes/App.js";
 document.addEventListener("DOMContentLoaded", () => {
     const app = new App();
 });
+
+
+// Smooth scrolling for elements with the 'smooth-scroll' class
+document.querySelectorAll('.smooth-scroll').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault(); // Prevent the default jump
+  
+      const targetId = this.getAttribute('href').substring(1); // Extract the ID without '#'
+      const targetElement = document.getElementById(targetId);
+  
+      if (targetElement) {
+        // Smoothly scroll to the section
+        targetElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+      }
+    });
+  });
+  
